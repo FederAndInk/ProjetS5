ae499be0cd607853a27890d54805970fc698b2d3
FederAndInk <federandink@gmail.com>
2018-12-16 16:15:52 +0100

    parse header + show it (waiting for elf reader) + add docs to README

--- src/ElfParser.c
+++ src/ElfParser.c
@@ -1,3 +1,30 @@
 #include "ElfParser.h"
+#include "ElfReader.h"
 
-void parseHeader(Elf e, Elf32_Ehdr* hdr) {}
+void parseHeader(Elf e, Elf32_Ehdr* hdr)
+{
+  elfGoTo(e, 0);
+
+  for (size_t i = 0; i < EI_NIDENT; i++)
+  {
+    hdr->e_ident[i] = elfReadUC(e);
+  }
+
+  hdr->e_type = elfRead16(e);
+  hdr->e_machine = elfRead16(e);
+  hdr->e_version = elfRead32(e);
+
+  if (hdr->e_ident[EI_CLASS] == ELFCLASS32)
+  {
+    hdr->e_entry = elfRead32(e);
+    hdr->e_phoff = elfRead32(e);
+    hdr->e_shoff = elfRead32(e);
+    hdr->e_flags = elfRead32(e);
+    hdr->e_ehsize = elfRead16(e);
+    hdr->e_phentsize = elfRead16(e);
+    hdr->e_phnum = elfRead16(e);
+    hdr->e_shentsize = elfRead16(e);
+    hdr->e_shnum = elfRead16(e);
+    hdr->e_shstrndx = elfRead16(e);
+  }
+}
--- src/ElfParser.h
+++ src/ElfParser.h
@@ -2,4 +2,11 @@
 #include "ElfReader.h"
 #include <linux/elf.h>
 
+/**
+ * @brief parse the header of the elf file e
+ * if e is not of class ELF32 the parsing stops before arch specific fields
+ * e_ident is valid
+ * @param e 
+ * @param hdr 
+ */
 void parseHeader(Elf e, Elf32_Ehdr* hdr);
