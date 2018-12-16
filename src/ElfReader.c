cda93dc73c7d493a0f99a8c6d3643929d09988cb
Lilian <lilian@Thinkpad>
2018-12-16 19:56:17 +0100

    elfopen, read32, read16

--- src/ElfReader.c
+++ src/ElfReader.c
@@ -1,14 +1,16 @@
 #include "ElfReader.h"
+#include "util.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 // Implementation function
 
 /**
  * @brief open f in mode m
  * do nothing if f is already in mode m
- * 
- * @param f 
- * @param m 
+ *
+ * @param f
+ * @param m
  */
 void ElfOpenIn(Elf f, ElfMode m);
 
@@ -16,24 +18,63 @@ void ElfOpenIn(Elf f, ElfMode m) {}
 
 // End Implementation function
 
-bool isElf(char const* file)
+bool isElf(Elf f)
 {
-  FILE* f = fopen(file, "r");
-  char  elf[5] = {0};
-  elf[0] = fgetc(f);
-  elf[1] = fgetc(f);
-  elf[2] = fgetc(f);
-  elf[3] = fgetc(f);
-  return elf[0] == 0x7f && elf[1] == 'E' && elf[2] == 'L' && elf[3] == 'F';
+  return f != NULL;
 }
 
-Elf elfOpen(char const* fileName) {}
+bool elfIsSameIndianess(Elf f)
+{
+  return (f->eiData == ELFDATA2MSB && is_big_endian()) ||
+         (f->eiData == ELFDATA2LSB && !is_big_endian());
+}
 
-uint32_t elfRead32(Elf f) {}
+Elf elfOpen(char const* fileName) {
+  Elf res = NULL;
+  res = malloc(sizeof(Elf));
+  if(res != NULL)
+  {
+    res->fileName = fileName ;
+    res->f = fopen(res->fileName, "rb");
+    if(res->f == NULL)
+    {
+      fprintf(stderr, "Erreur d'ouverture de fichier.\n");
+    }
+    res->openMode = READ;
+  }
+  return res;
+}
 
-uint16_t elfRead16(Elf f) {}
+uint32_t elfRead32(Elf f) {
+  uint32_t res = 0;
+  uint8_t c ;
+  int len = 4;
+  while(len--)
+  {
+    res <<= 8;
+    c = fgetc(f->f);
+    res = c | res ;
+  }
+  return res;
+}
 
-unsigned char elfReadUC(Elf f) {}
+uint16_t elfRead16(Elf f) {
+  uint32_t res = 0;
+  uint8_t c ;
+  int len = 2;
+  while(len--)
+  {
+    res <<= 8;
+    c = fgetc(f->f);
+    res = c | res ;
+  }
+  return res;
+}
+
+unsigned char elfReadUC(Elf f) {
+  unsigned char res = 0;
+  return res;
+}
 
 void elfWrite32(Elf f, uint32_t e) {}
 
@@ -45,4 +86,6 @@ void elfGoTo(Elf f, size_t to) {}
 
 void elfGoToRel(Elf f, size_t offset) {}
 
-void close(Elf f) {}
+void close(Elf f) {
+  fclose(f->f);
+}
