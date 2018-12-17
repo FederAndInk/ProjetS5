07fc294eb997fa4ebe3e0286058cb6245e64aa0a
Gary Prat <gary73@gmx.fr>
2018-12-17 16:28:17 +0100

    debut de elfparser pour sectionheader

--- src/ElfParser.h
+++ src/ElfParser.h
@@ -6,7 +6,19 @@
  * @brief parse the header of the elf file e
  * if e is not of class ELF32 the parsing stops before arch specific fields
  * e_ident is valid
- * @param e 
- * @param hdr 
+ * @param e
+ * @param hdr
  */
 void parseHeader(Elf e, Elf32_Ehdr* hdr);
+
+
+
+
+/*
+    Parse the section header of the elf file e
+
+    e must be a 32 bits elf file.
+    shdr must be a valid pointeur
+
+*/
+void parseSectionHeader(Elf e, Elf32_Shdr* shdr);
