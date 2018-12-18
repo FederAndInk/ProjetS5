69b1d58fc96bc82950cfbbc519a0b092b655331d
FederAndInk <federandink@gmail.com>
2018-12-18 15:19:12 +0100

    add coverage (codeCov)

--- launchTest.sh
+++ launchTest.sh
@@ -3,3 +3,22 @@
 cd ./build/test
 
 ctest -V
+
+cd ..
+
+rm -r coverage
+mkdir -p coverage
+
+covFile=`find . -name "*.gcda"`
+covFile="$covFile `find . -name "*.gcno"`"
+
+cp $covFile coverage
+
+if command -v gcovr; then
+  cd ..
+  cp build/coverage/* .
+  gcovr -r . --html-details -o build/coverage/index.html
+
+  rm *.gcda
+  rm *.gcno
+fi
