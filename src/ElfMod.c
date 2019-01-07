aeb8b2fd0a82bb8030230ad8f0e54c7fbf0cc44b
lordofkawaiii <thierry.duchaine@gmail.com>
2019-01-07 22:33:40 +0100

    [STABLE] pour toi ludo

--- src/ElfMod.c
+++ src/ElfMod.c
@@ -27,9 +27,8 @@ void writeSectionsInFile(ElfImageP elfI, Elf dest, Elf input)
   for (size_t i = 0; i < elfI->sections.size; i++)
   {
     elfGoTo(input, elfI->sections.tab[i].sh_offset);
-    elfWriteUC(dest, elfReadUC(input));
     elfI->sections.tab[i].sh_offset = elfTell(dest); // set new offset
-    for (size_t k = 1; k < elfI->sections.tab[i].sh_size; k++)
+    for (size_t k = 0; k < elfI->sections.tab[i].sh_size; k++)
     {
       elfWriteUC(dest, elfReadUC(input));
     }
@@ -37,4 +36,37 @@ void writeSectionsInFile(ElfImageP elfI, Elf dest, Elf input)
   }
 }
 
-void writeSSH(ElfImageP elfI, Elf dest, Elf input) {}
+void writeSectionHeaders(ElfImageP elfI, Elf dest, Elf input)
+{
+  elfI->sections.size = elfI->hdr.e_shnum;
+
+  elfI->sections.tab = (Elf32_Shdr*)malloc(elfI->sections.size * sizeof(Elf32_Shdr));
+  elfI->hdr.e_shoff = elfTell(dest);
+  for (Elf32_Word i = 0; i < elfI->sections.size; i++)
+  {
+    elfWrite32(dest, elfI->sections.tab[i].sh_name);
+    elfWrite32(dest, elfI->sections.tab[i].sh_type);
+    elfWrite32(dest, elfI->sections.tab[i].sh_addr);
+    elfWrite32(dest, elfI->sections.tab[i].sh_offset);
+    elfWrite32(dest, elfI->sections.tab[i].sh_size);
+    elfWrite32(dest, elfI->sections.tab[i].sh_link);
+    elfWrite32(dest, elfI->sections.tab[i].sh_info);
+    elfWrite32(dest, elfI->sections.tab[i].sh_addralign);
+    elfWrite32(dest, elfI->sections.tab[i].sh_entsize);
+  }
+}
+
+void modifyElfHeader(ElfImageP elfI, Elf dest)
+{
+  //modify e_phoff
+  elfGoTo(dest, 0x1c);
+  //TODO set e_phoff
+
+  //modify e_shstrndx
+  elfGoTo(dest, 0x30);
+  //elfWrite16(dest, )
+
+  //modify e_shnum
+  elfGoTo(dest, 0x30);
+  elfWrite16(dest, elfI->sections.size);
+}
